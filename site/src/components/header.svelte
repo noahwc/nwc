5e4cffba30ae71507623d31183919f410aeb6481
Noah Clarke <noahwclarke@gmail.com>
2024-11-17 22:50:33 -0800

    Header style improvements

--- site/src/components/header.svelte
+++ site/src/components/header.svelte
@@ -1,14 +1,34 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	interface NavItem {
+		url: string;
+		label: string;
+	}
+
+	const links: NavItem[] = [
+		{ url: '/', label: 'Home' },
+		{ url: '/blog', label: 'Blog' },
+		{ url: '/gallery', label: 'Pictures' },
+		{ url: '/about', label: 'About' }
+	];
+</script>
+
 <nav>
-	<a href="/">Home</a>
-	<a href="/blog">Blog</a>
-	<a href="/gallery">Pictures</a>
-	<a href="/about">About</a>
+	{#each links as link}
+		<a class:active={$page.url.pathname === link.url} href={link.url}>{link.label}</a>
+	{/each}
 </nav>
 
 <style>
 	nav {
 		display: flex;
 		gap: var(--space-x-large);
-		padding: var(--space-medium) 0;
+		padding: var(--space-x-large) 0 var(--space-large);
+		font-size: var(--text-size);
+	}
+
+	a.active {
+		text-decoration: underline;
 	}
 </style>
